86633b2e95b68abdb440e11f764a8cae300260a1
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-12-10 10:05:17 -0300

    -feat: configs;

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AppRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
