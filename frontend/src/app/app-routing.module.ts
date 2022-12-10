3a4896e538b94cddb2ae8b75d493b6c2afac4560
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-12-10 11:49:28 -0300

    -config: routing; -feat: home boostrap; -feat: components bootstrap; -feat: services boostrap;

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConfigsComponent } from './pages/configs/configs.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthGuard } from './utils/auth.guard';
 
 const routes: Routes = [
-
-
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'configs',
+        children: [
+          {
+            path: '',
+            component: ConfigsComponent,
+          }
+        ]
+      }
+    ]
+  },
+  { 
+    path: '**', 
+    redirectTo: '',
+    pathMatch: 'full'
+  },
 ]
 
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthGuard } from './utils/auth.guard';
+import { ComponentsModule } from './components/components.module';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ConfigsComponent } from './pages/configs/configs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ConfigsComponent,
   ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
+    AppRoutingModule,
+    ComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
