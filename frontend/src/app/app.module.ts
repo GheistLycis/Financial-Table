3e5ad11d37c05c4a6f5a43c7b1de77bdfd1ea43b
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-12-10 10:33:09 -0300

    -config: angular core update and deps installation.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
 
 import { AppComponent } from './app.component';
-import { AppRoutes } from './app.routing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
