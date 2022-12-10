3afa31d70614cd67188ccb88a30011f8db894dbe
Bruno Capitelli <bruno.nitro48@gmail.com>
2022-12-10 17:12:40 -0300

    -config: set project to use scss instead of css; -feat: custom cursor;

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('cursorDot') cursorDot
+  @ViewChild('cursorCircle') cursorCircle
+
   constructor() { }
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void { }
+
+  test() {
+    console.log(this.cursorCircle)
+  }
+
+  @HostListener('document:mousemove', ['$event']) 
+  onMouseMove(e: MouseEvent) {
+    this.cursorDot.nativeElement.style.left = e.clientX - 5 + 'px'
+    this.cursorDot.nativeElement.style.top = e.clientY - 50 + 'px'
+  
+    setTimeout(() => {
+      this.cursorCircle.nativeElement.style.left = e.clientX - 6 + 'px'
+      this.cursorCircle.nativeElement.style.top = e.clientY - 7 + 'px'
+    }, 150)
   }
 }
--- frontend/src/app/pages/configs/configs.component.ts
+++ frontend/src/app/pages/configs/configs.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-configs',
   templateUrl: './configs.component.html',
-  styleUrls: ['./configs.component.css']
+  styleUrls: ['./configs.component.scss']
 })
 export class ConfigsComponent {
 
