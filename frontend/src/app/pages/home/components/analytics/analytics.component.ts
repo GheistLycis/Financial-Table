502acca8831a7396c3d3c4c2d900ab729c5b3aa3
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-06-23 19:28:15 -0300

    -refactor: minor;

--- frontend/src/app/pages/home/components/analytics/analytics.component.ts
+++ frontend/src/app/pages/home/components/analytics/analytics.component.ts
@@ -20,24 +20,20 @@ export class AnalyticsComponent implements OnInit {
   constructor(
     private yearService: YearService,
     private monthService: MonthService,
-  ) { }
-  
-  ngOnInit(): void {
+  ) {
     this.year$.subscribe(value => this.monthService.list({ year: value.id }).subscribe(({ data }) => this.months$.next(data)))
     
     this.months$.subscribe(value => this.month$.next(value[0]))
     
     this.month$.subscribe(() => this.calculateAnalytics())
-    
+  }
+  
+  ngOnInit(): void {
     this.yearService.list().subscribe(({ data }) => {
       this.years = data
       
       this.year$.next(this.years[0])
     })
-  }
-  
-  toggleSelect(option: 'year' | 'month'): void {
-    this.dropdown = this.dropdown == option ? '' : option
   }
   
   calculateAnalytics(): void {
