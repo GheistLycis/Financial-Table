ac601ff76373ed3b61a340536e28fb1d44b95792
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-06-23 17:42:52 -0300

    -refacto: analytics header -chore: updating insomnia file;

--- frontend/src/app/pages/home/components/analytics/analytics.component.ts
+++ frontend/src/app/pages/home/components/analytics/analytics.component.ts
@@ -3,7 +3,7 @@ import MonthDTO from 'src/app/DTOs/month';
 import YearDTO from 'src/app/DTOs/year';
 import { MonthService } from 'src/app/services/month/month.service';
 import { YearService } from 'src/app/services/year/year.service';
-import { Observable, map, tap, switchMap } from 'rxjs';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-analytics',
@@ -12,32 +12,28 @@ import { Observable, map, tap, switchMap } from 'rxjs';
 })
 export class AnalyticsComponent implements OnInit {
   dropdown: '' | 'year' | 'month' = ''
-  years$!: Observable<YearDTO[]>
-  months$!: Observable<MonthDTO[]>
-  year!: YearDTO
-  month!: MonthDTO
+  years: YearDTO[] = []
+  year$ = new Subject<YearDTO>()
+  months$ = new Subject<MonthDTO[]>()
+  month$ = new Subject<MonthDTO>()
   
   constructor(
     private yearService: YearService,
     private monthService: MonthService,
-  ) {
-    this.years$ = this.yearService.list().pipe(
-      map(res => res.data),
-      tap(([ lastestYear ]) => {
-        this.year = lastestYear
-        
-        this.months$ = this.monthService.list({ year: lastestYear.id }).pipe(
-          map(res => res.data),
-          tap(([ latestMonth ]) => this.month = latestMonth),
-        )
-        
-        this.months$.subscribe()
-      }),
-    )
-  }
+  ) { }
   
   ngOnInit(): void {
-    this.years$.subscribe()
+    this.year$.subscribe(value => this.monthService.list({ year: value.id }).subscribe(({ data }) => this.months$.next(data)))
+    
+    this.months$.subscribe(value => this.month$.next(value[0]))
+    
+    this.month$.subscribe(() => this.calculateAnalytics())
+    
+    this.yearService.list().subscribe(({ data }) => {
+      this.years = data
+      
+      this.year$.next(this.years[0])
+    })
   }
   
   toggleSelect(option: 'year' | 'month'): void {
