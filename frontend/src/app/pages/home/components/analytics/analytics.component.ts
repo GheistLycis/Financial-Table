29b17cdaee70dd9d3978928e75885326c75ab870
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-06-24 09:16:03 -0300

    -feat: calculating recent expenses analytics;

--- frontend/src/app/pages/home/components/analytics/analytics.component.ts
+++ frontend/src/app/pages/home/components/analytics/analytics.component.ts
@@ -3,7 +3,8 @@ import MonthDTO from 'src/app/DTOs/month';
 import YearDTO from 'src/app/DTOs/year';
 import { MonthService } from 'src/app/services/month/month.service';
 import { YearService } from 'src/app/services/year/year.service';
-import { Subject } from 'rxjs';
+import { Subject, BehaviorSubject, forkJoin, map, firstValueFrom } from 'rxjs';
+import { ExpenseService } from 'src/app/services/expense/expense.service';
 
 @Component({
   selector: 'app-analytics',
@@ -14,12 +15,14 @@ export class AnalyticsComponent implements OnInit {
   dropdown: '' | 'year' | 'month' = ''
   years: YearDTO[] = []
   year$ = new Subject<YearDTO>()
-  months$ = new Subject<MonthDTO[]>()
-  month$ = new Subject<MonthDTO>()
+  months$ = new BehaviorSubject<MonthDTO[]>(null)
+  month$ = new BehaviorSubject<MonthDTO>(null)
+  recentExpenses: number | '--' = '--'
   
   constructor(
     private yearService: YearService,
     private monthService: MonthService,
+    private expenseService: ExpenseService,
   ) {
     this.year$.subscribe(value => this.monthService.list({ year: value.id }).subscribe(({ data }) => this.months$.next(data)))
     
@@ -37,6 +40,31 @@ export class AnalyticsComponent implements OnInit {
   }
   
   calculateAnalytics(): void {
+    this.calculateRecentExpenses()
+  }
+  
+  async calculateRecentExpenses(): Promise<void> {
+    const actualMonth = this.month$.getValue()
+    let previousMonth: MonthDTO
+    
+    if(actualMonth.month == 1) {
+      const previousYear = this.years.find(({ year }) => year == actualMonth.year.year-1)
+      
+      previousMonth = await firstValueFrom(this.monthService.list({ year: previousYear.id })).then(({ data }) => data[0])
+    }
+    else {
+      previousMonth = this.months$.getValue().find(({ month, year }) => (month == actualMonth.month-1) && (year.year == actualMonth.year.year))
+    }
 
+    const lastMonthsExpenses$ = forkJoin({
+      actualMonth: this.expenseService.list({ month: actualMonth.id }).pipe(map(res => res.data.reduce((prev, curr) => prev += curr.value, 0))),
+      previousMonth: this.expenseService.list({ month: previousMonth.id }).pipe(map(res => res.data.reduce((prev, curr) => prev += curr.value, 0))),
+    })
+    
+    lastMonthsExpenses$.subscribe(({ actualMonth, previousMonth }) => {
+      this.recentExpenses = actualMonth && previousMonth 
+        ? ((100 * actualMonth / previousMonth) -100)
+        : '--'
+    })
   }
 }
