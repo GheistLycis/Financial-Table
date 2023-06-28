0cd05211b4e0e9b2ebce434843d72557031a1294
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-06-28 15:52:12 -0300

    -feat: initing table;

--- frontend/src/app/pages/management/management.module.ts
+++ frontend/src/app/pages/management/management.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManagementRoutingModule } from './management-routing.module';
+import { YearsComponent } from './years/years.component';
+import { MonthsComponent } from './months/months.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { GroupsComponent } from './groups/groups.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    YearsComponent,
+    MonthsComponent,
+    CategoriesComponent,
+    GroupsComponent,
+  ],
   imports: [
     CommonModule,
     ManagementRoutingModule,
--- frontend/src/app/pages/management/years/years.component.ts
+++ frontend/src/app/pages/management/years/years.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import YearDTO from 'src/app/shared/DTOs/year';
+import { ExpenseService } from 'src/app/shared/services/expense/expense.service';
+import { YearService } from 'src/app/shared/services/year/year.service';
+
+
+type yearAnalytics = {
+  year: YearDTO
+  available: number
+  incomes: number
+  fixedExpenses: number
+  variableExpenses: number
+}
 
 @Component({
   selector: 'app-years',
   templateUrl: './years.component.html',
   styleUrls: ['./years.component.scss']
 })
-export class YearsComponent {
-
+export class YearsComponent implements OnInit {
+  yearsAnalytics$ = new BehaviorSubject<yearAnalytics[]>([])
+  
+  constructor(
+    private yearService: YearService,
+    private expenseService: ExpenseService,
+  ) { }
+  
+  ngOnInit(): void {
+    // this.yearService.list().pipe(
+    //   map(({ data }) => {
+    //     const requests = data.map(({ year, id }) => {
+    //       return {
+    //         year,
+    //         request: this.expenseService.list({ year: id })
+    //       }
+    //     })
+    //   })
+    // ).subscribe({ next: data => this.yearsAnalytics$.next([ ...this.yearsAnalytics$.getValue(), data ])})
+  }
+  
+  addYear(): void {
+    
+  }
 }
