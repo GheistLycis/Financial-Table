38ea5e46f75786b2b206e0f8f13981b5983ce445
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-06-29 12:49:00 -0300

    -feat: requesting yearsHistories;

--- frontend/src/app/pages/management/years/years.component.ts
+++ frontend/src/app/pages/management/years/years.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, concatMap, from, map, switchMap } from 'rxjs';
 import YearHistory from 'src/app/shared/interfaces/YearHistory';
+import { AnalyticsService } from 'src/app/shared/services/analytics/analytics.service';
+import { YearService } from 'src/app/shared/services/year/year.service';
 
 
 @Component({
@@ -11,10 +13,25 @@ import YearHistory from 'src/app/shared/interfaces/YearHistory';
 export class YearsComponent implements OnInit {
   yearsHistories$ = new BehaviorSubject<YearHistory[]>([])
   
-  constructor() { }
+  constructor(
+    private yearService: YearService,
+    private analyticsService: AnalyticsService,
+  ) { }
   
   ngOnInit(): void {
-
+    const years$ = this.yearService.list().pipe(
+      switchMap(({ data }) => from(data))
+    )
+    
+    const histories$ = years$.pipe(
+      concatMap(({ id }) => this.analyticsService.yearHistory(id).pipe(
+        map(({ data }) => data)
+      ))
+    )
+    
+    histories$.pipe(
+      map(history => this.yearsHistories$.getValue().concat(history))
+    ).subscribe(this.yearsHistories$)
   }
   
   addYear(): void {
