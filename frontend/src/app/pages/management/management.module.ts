1f16807fddc445f1896c9964e8c45201e7e51647
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-07-03 11:14:18 -0300

    -fix: module imports;

--- frontend/src/app/pages/management/management.module.ts
+++ frontend/src/app/pages/management/management.module.ts
@@ -3,8 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ManagementRoutingModule } from './management-routing.module';
 import { YearsModule } from './years/years.module';
 import { MonthsModule } from './months/months.module';
-import { GroupsModule } from './groups/groups.module';
-import { CategoriesModule } from './categories/categories.module';
 
 
 @NgModule({
@@ -14,8 +12,6 @@ import { CategoriesModule } from './categories/categories.module';
     ManagementRoutingModule,
     YearsModule,
     MonthsModule,
-    CategoriesModule,
-    GroupsModule,
   ]
 })
 export class ManagementModule { }
