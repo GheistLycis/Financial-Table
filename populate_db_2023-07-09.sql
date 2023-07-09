7a7cf0f3c3c898c495c657b9a66cbc247824a79b
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-07-09 14:41:28 -0300

    -chore: updating sql file; -chore: updating Insomnia file;

--- populate_db_2023-07-09.sql
+++ populate_db_2023-07-09.sql
@@ -1,5 +1,5 @@
 TRUNCATE TABLE 
-  ips, users, expenses_tags_tags, tags, expenses, categories, monthly_expenses, monthly_incomes, months, years
+  ips_users_users, ips, users, expenses_tags_tags, tags, expenses, categories, monthly_expenses, monthly_incomes, months, years
   RESTART IDENTITY;
 
 INSERT INTO years ("createdAt","updatedAt","deletedAt","year") VALUES
