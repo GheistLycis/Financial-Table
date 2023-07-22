11295c6263f81f7adeedf35ccbcf73ae285024bc
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-07-21 23:58:29 -0300

    -style: footer;

--- frontend/src/app/layouts/default-layout/components/footer/footer.component.ts
+++ frontend/src/app/layouts/default-layout/components/footer/footer.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  easterEgg = ''
 
+  showEasterEgg(): void {
+    for(let i = 0; i < 3; i++) {
+      setTimeout(() => this.easterEgg += '.', 500 * i)
+    }
+
+    setTimeout(() => this.easterEgg += ' =]', 2000)
+
+    setTimeout(() => this.easterEgg = '', 4000)
+  }
 }
