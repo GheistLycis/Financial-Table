4747710e7c6b0f9ee3b16f8f834803e5e77e0cb2
Bruno Capitelli <bruno.nitro48@gmail.com>
2023-08-02 16:07:00 -0300

    -commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ import { ResponseHandlerInterceptor } from "./interceptors/response-handler/resp
 declare const module: any // webpack's hot-reload
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule, { cors: { origin: 'https://financial-table.vercel.app' } })
   const config = new DocumentBuilder().build()
   const document = SwaggerModule.createDocument(app, config)
 
